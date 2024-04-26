17a36e6c447545f8c22f0c84ed952346b6e8cf04
Jhonatan <jhonatan.dev90@gmail.com>
2024-04-26 16:23:30 -0300

    Criando uma função para capturar os valores dos inputs e exibi-los na tela.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 // Criando uma div com o id "campinho"
 var campinhoDiv = document.createElement("div");
 campinhoDiv.id = "campinho";
+campinhoDiv.style.position = "relative";
 // Criando um elemento img (capinho)
 var imgElement = document.createElement("img");
 // Definindo o atributo src com o caminho da imagem
@@ -14,6 +15,12 @@ document.getElementById("main").insertBefore(campinhoDiv, referenceNode);
 
 // ----------------------------------------------------------------
 // Função para capturar os valores dos inputs
+// Variáveis para rastrear se as informações foram escolhidas
+var esquemaEscolhido = false;
+var tecnicoEscolhido = false;
+var jogadoresEscolhidos = 0;
+
+// Função para capturar os valores dos inputs e exibi-los na tela
 function capturarValoresInputs() {
   // Capturando os valores dos inputs
   var esquema = document.getElementById("esquema").value;
@@ -23,16 +30,41 @@ function capturarValoresInputs() {
   var tecnico = document.getElementById("tecnico").value;
 
   // Exibindo os valores na console
-  console.log("Esquema Tático:", esquema);
-  console.log("Nome do Jogador:", nomeJogador);
-  console.log("Número do Jogador:", numeroJogador);
-  console.log("Posição:", posicao);
-  console.log("Nome do Técnico:", tecnico);
-}
+  // console.log("Esquema Tático:", esquema);
+  // console.log("Nome do Jogador:", nomeJogador);
+  // console.log("Número do Jogador:", numeroJogador);
+  // console.log("Posição:", posicao);
+  // console.log("Nome do Técnico:", tecnico);
 
+  // Criando divs separadas para exibir as informações
+  if (!esquemaEscolhido) {
+    var esquemaDiv = document.createElement("div");
+    esquemaDiv.textContent = "Esquema Tático: " + esquema;
+    campinhoDiv.appendChild(esquemaDiv);
+    esquemaEscolhido = true;
+  }
+
+  if (!tecnicoEscolhido) {
+    var tecnicoDiv = document.createElement("div");
+    tecnicoDiv.textContent = "Técnico: " + tecnico;
+    campinhoDiv.appendChild(tecnicoDiv);
+    tecnicoEscolhido = true;
+  }
+
+  if (jogadoresEscolhidos < 11) {
+    var jogadorDiv = document.createElement("div");
+    jogadorDiv.textContent =
+      "Nome: " +
+      nomeJogador +
+      ", Número: " +
+      numeroJogador +
+      ", Posição: " +
+      posicao;
+    campinhoDiv.appendChild(jogadorDiv);
+    jogadoresEscolhidos++;
+  }
+}
 // Adicionando um evento de clique ao botão "Adicionar"
-document
-  .querySelector(".btn-add button")
-  .addEventListener("click", function () {
-    capturarValoresInputs(); // Chamando a função para capturar os valores dos inputs quando o botão é clicado
-  });
+document.querySelector(".btn-add button").addEventListener("click", () => {
+  capturarValoresInputs(); // Chamando a função para capturar os valores dos inputs quando o botão é clicado
+});
